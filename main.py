970d25b274fc22caca8e960828ca242a2b5e4942
DESKTOP-1RATK81\andrew <piscoiandrei27@gmail.com>
2020-09-27 15:50:45 +0300

    mhm

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from my_decorators.my_wrappers import *
 
 
+@timer
+@debug
+def some_function(x):
+    k = 0
+    for i in range(5, x):
+        k += ((i / (i + 1) - i / i + i + i - i / (i - 1)) * 0.000003) * i
+    return k
 
+
+print(some_function(10))
---
+++ my_decorators/my_wrappers.py
@@ -0,0 +1,30 @@
+import time
+import functools
+
+
+def timer(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        ret_val = func(*args, **kwargs)
+        total = time.time() - start
+        print("Execution time: ", total)
+        return ret_val
+
+    return wrapper
+
+
+def debug(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        args_repr = [repr(a) for a in args]
+        kwargs_repr = [f"{k}={v}" for k, v in kwargs.items()]
+        signature = ", ".join(args_repr + kwargs_repr)
+        print(f"Calling {func.__name__}({signature})")
+
+        result = func(*args, **kwargs)
+
+        print(f"{func.__name__} returned {result}")
+        return result
+
+    return wrapper

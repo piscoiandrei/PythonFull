1b58827a0c4a95ce42ea9d3a9637508344a924e6
DESKTOP-1RATK81\andrew <piscoiandrei27@gmail.com>
2020-09-28 15:51:19 +0300

    mhm

--- context_mgrs.py
+++ context_mgrs.py
@@ -1,2 +1,47 @@
-with open("text.txt", 'w') as file:
-    file.write('Writing some text using contextmanager')
+from contextlib import contextmanager
+
+
+class ManagedFile:  # custom context manager # no library needed for this
+    def __init__(self, filename):
+        print("init")
+        self.filename = filename
+
+    def __enter__(self):
+        print("enter")
+        self.file = open(self.filename, 'w')
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.file:
+            self.file.close()
+
+        # after you handled the exeption return True so that the 'with'
+        # statement won't raise the exception
+        if exc_type is not None:
+            print("Exeption has been handled")
+
+        print("exit")
+        return True
+
+
+"""
+from context_mgrs import *
+
+with ManagedFile("text.txt") as my_file:
+    print("do somethingggg")
+    my_file.write("some textfgergerger")
+"""
+
+
+# CONTEXT MANAGER USING FUNCTION
+
+def open_managed_file(filename):
+    f = open(filename, 'w')
+    try:
+        yield f
+    finally:
+        f.close()
+
+
+with open_managed_file("notes.txt") as fl:
+    fl.write("something to do.....")
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 # OTHER TIPS AND TRCIKS
 # TODO ACTUAL COPYING aka DEEPCOPY
-'''
+"""
 import copy
 
 obj1 = SomeOject
 cpy = copy.deepcopy(obj1)  # used for nested stuff or normal
 # copy.deepcopy() can be used on any object
 
-'''
+"""
 from context_mgrs import *
+
+with ManagedFile("text.txt") as my_file:
+    print("do somethingggg")
+    my_file.write("some textfgergerger")
+    my_file.wrong()
